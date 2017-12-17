f03bf8890949f07adc038ca3cb974898037d4ce0
Madalyn Coryea <madalyn@Madalyns-MacBook-Pro-2.local>
2017-12-17 18:49:13 -0500

    Started fixing/adding the theming

--- Droid/Resources/Resource.designer.cs
+++ Droid/Resources/Resource.designer.cs
@@ -44,26 +44,26 @@ namespace GroupDJ.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050006
-			public const int downVoteButton = 2131034118;
+			// aapt resource value: 0x7f060006
+			public const int downVoteButton = 2131099654;
 			
-			// aapt resource value: 0x7f050001
-			public const int linearLayout1 = 2131034113;
+			// aapt resource value: 0x7f060001
+			public const int linearLayout1 = 2131099649;
 			
-			// aapt resource value: 0x7f050002
-			public const int playlistListView = 2131034114;
+			// aapt resource value: 0x7f060002
+			public const int playlistListView = 2131099650;
 			
-			// aapt resource value: 0x7f050003
-			public const int rankingTextView = 2131034115;
+			// aapt resource value: 0x7f060003
+			public const int rankingTextView = 2131099651;
 			
-			// aapt resource value: 0x7f050000
-			public const int searchView1 = 2131034112;
+			// aapt resource value: 0x7f060000
+			public const int searchView1 = 2131099648;
 			
-			// aapt resource value: 0x7f050004
-			public const int titleTextView = 2131034116;
+			// aapt resource value: 0x7f060004
+			public const int titleTextView = 2131099652;
 			
-			// aapt resource value: 0x7f050005
-			public const int upVoteButton = 2131034117;
+			// aapt resource value: 0x7f060005
+			public const int upVoteButton = 2131099653;
 			
 			static Id()
 			{
@@ -125,6 +125,22 @@ namespace GroupDJ.Droid
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int GroupDJTheme = 2131034112;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
 			{
 			}
 		}
